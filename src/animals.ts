5fe70dd716a70d460604e917428c263ca588f1b1
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-12-14 14:25:52 +0100

    Automated animals generation

--- src/animals.ts
+++ src/animals.ts
@@ -1,4 +1,5 @@
 import { createImageElement } from "./assets";
+import { capitalizeTyped } from "./types";
 
 export const stageIds = [
   "baby",
@@ -9,13 +10,13 @@ export type AnimalStageId = (typeof stageIds)[number];
 
 export type AnimalStage = {
   name: string;
-  sprite: HTMLImageElement | undefined;
+  sprite: HTMLImageElement;
   width: number;
   height: number;
 };
 
 export type Animal = {
-  stages: Record<AnimalStageId, AnimalStage | undefined>;
+  stages: Record<AnimalStageId, AnimalStage>;
 };
 
 export const secretAnimalIds = ["missing"] as const satisfies readonly string[];
@@ -33,225 +34,48 @@ export const animalIds = [
 ] as const satisfies readonly string[];
 export type AnimalID = (typeof animalIds)[number];
 
-export const animals = {
-  missing: {
+const isOverlap = animalIds.some((id: string) =>
+  secretAnimalIds.some((secretId) => secretId === id)
+);
+if (isOverlap) {
+  throw new Error("AnimalIDs and SecretAnimalIDs must be mutually exclusive");
+}
+
+function createAnimalProps({ id }: { id: AnimalID | SecretAnimalID }): Animal {
+  return {
     stages: {
       baby: {
-        name: "Missing (baby)",
-        sprite: createImageElement("/animals/missing.png"),
+        name: `Baby ${capitalizeTyped(id)}`,
+        sprite: createImageElement(`/animals/${id}-baby.png`),
         width: 16,
         height: 16,
       },
       adult: {
-        name: "Missing",
-        sprite: createImageElement("/animals/missing.png"),
+        name: capitalizeTyped(id),
+        sprite: createImageElement(`/animals/${id}.png`),
         width: 16,
         height: 16,
       },
       cool: {
-        name: "Missing (cool)",
-        sprite: createImageElement("/animals/missing.png"),
+        name: `Cool ${capitalizeTyped(id)}`,
+        sprite: createImageElement(`/animals/${id}-cool.png`),
         width: 16,
         height: 16,
       },
     },
-  },
-  robovac: {
-    stages: {
-      baby: {
-        name: "Tiny Robovac",
-        sprite: createImageElement("/animals/robovac-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Robovac",
-        sprite: createImageElement("/animals/robovac.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Robovac (with knife)",
-        sprite: createImageElement("/animals/robovac-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  bee: {
-    stages: {
-      baby: {
-        name: "Baby Bee",
-        sprite: createImageElement("/animals/bee-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Bee",
-        sprite: createImageElement("/animals/bee.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Queen",
-        sprite: createImageElement("/animals/bee-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  penguin: {
-    stages: {
-      baby: {
-        name: "Baby Penguin",
-        sprite: createImageElement("/animals/penguin-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Penguin",
-        sprite: createImageElement("/animals/penguin.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Cool Penguin",
-        sprite: createImageElement("/animals/penguin-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  cat: {
-    stages: {
-      baby: {
-        name: "Kitten",
-        sprite: createImageElement("/animals/cat-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Cat",
-        sprite: createImageElement("/animals/cat.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Cool Cat",
-        sprite: createImageElement("/animals/cat-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  dog: {
-    stages: {
-      baby: {
-        name: "Puppy",
-        sprite: createImageElement("/animals/dog-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Dog",
-        sprite: createImageElement("/animals/dog.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Dawg",
-        sprite: createImageElement("/animals/dog-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  raccoon: {
-    stages: {
-      baby: {
-        name: "Baby Raccoon",
-        sprite: createImageElement("/animals/raccoon-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Raccoon",
-        sprite: createImageElement("/animals/raccoon.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Cool Raccoon",
-        sprite: createImageElement("/animals/raccoon-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  duck: {
-    stages: {
-      baby: {
-        name: "Duckling",
-        sprite: createImageElement("/animals/duck-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Duck",
-        sprite: createImageElement("/animals/duck.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Cool Duck",
-        sprite: createImageElement("/animals/duck-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  swan: {
-    stages: {
-      baby: {
-        name: "Cygnet",
-        sprite: createImageElement("/animals/swan-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Swan",
-        sprite: createImageElement("/animals/swan.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Black Swan",
-        sprite: createImageElement("/animals/swan-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-  platypus: {
-    stages: {
-      baby: {
-        name: "Baby Platypus",
-        sprite: createImageElement("/animals/platypus-baby.png"),
-        width: 16,
-        height: 16,
-      },
-      adult: {
-        name: "Platypus",
-        sprite: createImageElement("/animals/platypus.png"),
-        width: 16,
-        height: 16,
-      },
-      cool: {
-        name: "Cool Platypus",
-        sprite: createImageElement("/animals/platypus-cool.png"),
-        width: 16,
-        height: 16,
-      },
-    },
-  },
-} as const satisfies Record<AnimalID | SecretAnimalID, Animal>;
+  };
+}
+
+const temp: Partial<Record<AnimalID | SecretAnimalID, Animal>> = {};
+
+for (const id of animalIds) {
+  temp[id] = createAnimalProps({ id });
+}
+
+for (const id of secretAnimalIds) {
+  temp[id] = createAnimalProps({ id });
+}
+
+export const animals = Object.freeze(
+  temp as Record<AnimalID | SecretAnimalID, Animal>
+);
---
+++ src/types.ts
@@ -0,0 +1,65 @@
+export function isEnumValue<E extends Record<string, string | number>>(
+  str: unknown,
+  enumeration: E
+): str is E[keyof E] {
+  return Object.values(enumeration).some((t) => t === str);
+}
+
+export function capitalizeTyped<T extends string>(str: T): Capitalize<T> {
+  return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
+}
+
+export function uncapitalizeTyped<T extends string>(str: T): Uncapitalize<T> {
+  return (str.charAt(0).toLowerCase() + str.slice(1)) as Uncapitalize<T>;
+}
+
+export function lowercaseTyped<T extends string>(str: T): Lowercase<T> {
+  return str.toLowerCase() as Lowercase<T>;
+}
+
+export function uppercaseTyped<T extends string>(str: T): Uppercase<T> {
+  return str.toUpperCase() as Uppercase<T>;
+}
+
+/**
+ * Typed version of Object.keys()
+ *
+ * A looser implementation than `(keyof T)[]`, using a string suggestion hack:
+ *
+ * ```ts
+ * (keyof T | (string & {}))[]
+ * ```
+ *
+ * Which allows for autocomplete features, without falsely enforcing a strict set of keys
+ * (additional properties can always be added to non-constant objects)
+ */
+export function objectKeys<T extends Record<string, unknown>>(
+  obj: T
+  // eslint-disable-next-line @typescript-eslint/ban-types -- Need for hack
+): (keyof T | (string & {}))[] {
+  return Object.keys(obj);
+}
+
+type Separator = " " | "-";
+
+export type Trim<
+  T extends string,
+  Acc extends string = ""
+> = T extends `${infer Char}${infer Rest}`
+  ? Char extends Separator
+    ? Trim<Rest, Acc>
+    : Trim<Rest, `${Acc}${Char}`>
+  : T extends ""
+  ? Acc
+  : never;
+
+/**
+ * Removes all spaces from a string
+ */
+export function trimTyped<T extends string>(str: T): Trim<T> {
+  return str.replaceAll(" ", "") as Trim<T>;
+}
+
+export function isRecord(value: unknown): value is Record<string, unknown> {
+  return typeof value === "object" && value !== null && !Array.isArray(value);
+}
