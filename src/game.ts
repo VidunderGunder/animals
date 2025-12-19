45deb6f9dbdbcdbc575fa28d4d82f944da4080bc
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-12-19 20:40:23 +0100

    cleanup

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,3 @@
-import { speak } from "./audio/speak";
 import { FPS_LIMIT } from "./config";
 import { laptop, openLaptop } from "./scenes/laptop/laptop";
 import { overworld } from "./scenes/overworld";
