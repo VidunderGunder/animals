f62653a719b5d8b2b9422d02b48ef4f6368f881a
Kristian Gunder Kramås <kristiankramas@outlook.com>
2026-01-12 17:24:15 +0100

    dot naming for ts and css specific folders

--- src/.typescript/types.ts
+++ src/.typescript/types.ts
@@ -1,24 +1,24 @@
 export function isEnumValue<E extends Record<string, string | number>>(
-  str: unknown,
-  enumeration: E
+	str: unknown,
+	enumeration: E,
 ): str is E[keyof E] {
-  return Object.values(enumeration).some((t) => t === str);
+	return Object.values(enumeration).some((t) => t === str);
 }
 
 export function capitalizeTyped<T extends string>(str: T): Capitalize<T> {
-  return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
+	return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
 }
 
 export function uncapitalizeTyped<T extends string>(str: T): Uncapitalize<T> {
-  return (str.charAt(0).toLowerCase() + str.slice(1)) as Uncapitalize<T>;
+	return (str.charAt(0).toLowerCase() + str.slice(1)) as Uncapitalize<T>;
 }
 
 export function lowercaseTyped<T extends string>(str: T): Lowercase<T> {
-  return str.toLowerCase() as Lowercase<T>;
+	return str.toLowerCase() as Lowercase<T>;
 }
 
 export function uppercaseTyped<T extends string>(str: T): Uppercase<T> {
-  return str.toUpperCase() as Uppercase<T>;
+	return str.toUpperCase() as Uppercase<T>;
 }
 
 /**
@@ -34,32 +34,32 @@ export function uppercaseTyped<T extends string>(str: T): Uppercase<T> {
  * (additional properties can always be added to non-constant objects)
  */
 export function objectKeys<T extends Record<string, unknown>>(
-  obj: T
-  // eslint-disable-next-line @typescript-eslint/ban-types -- Need for hack
+	obj: T,
+	// eslint-disable-next-line @typescript-eslint/ban-types -- Need for hack
 ): (keyof T | (string & {}))[] {
-  return Object.keys(obj);
+	return Object.keys(obj);
 }
 
 type Separator = " " | "-";
 
 export type Trim<
-  T extends string,
-  Acc extends string = ""
+	T extends string,
+	Acc extends string = "",
 > = T extends `${infer Char}${infer Rest}`
-  ? Char extends Separator
-    ? Trim<Rest, Acc>
-    : Trim<Rest, `${Acc}${Char}`>
-  : T extends ""
-  ? Acc
-  : never;
+	? Char extends Separator
+		? Trim<Rest, Acc>
+		: Trim<Rest, `${Acc}${Char}`>
+	: T extends ""
+		? Acc
+		: never;
 
 /**
  * Removes all spaces from a string
  */
 export function trimTyped<T extends string>(str: T): Trim<T> {
-  return str.replaceAll(" ", "") as Trim<T>;
+	return str.replaceAll(" ", "") as Trim<T>;
 }
 
 export function isRecord(value: unknown): value is Record<string, unknown> {
-  return typeof value === "object" && value !== null && !Array.isArray(value);
+	return typeof value === "object" && value !== null && !Array.isArray(value);
 }
