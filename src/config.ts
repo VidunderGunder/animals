531faeba33fa17debd0fac2e9d14fd723219675d
Kristian Gunder Kramås <kristiankramas@outlook.com>
2026-01-19 11:01:59 +0100

    Update DEBUG_OVERLAY to reflect development environment setting

--- src/config.ts
+++ src/config.ts
@@ -84,7 +84,7 @@ export const movementSpeeds = {
 	run: 0.13,
 } as const satisfies Record<MovementType, number>;
 
-export const DEBUG_OVERLAY = true;
+export const DEBUG_OVERLAY = import.meta.env.DEV;
 
 function isValidDimensions() {
 	if (Number.isInteger(GAME_WIDTH_PX) && Number.isInteger(GAME_HEIGHT_PX)) {
